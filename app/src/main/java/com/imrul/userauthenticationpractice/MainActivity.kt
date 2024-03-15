93ccac96bf5a0735a33c9a7bec8d82d7234aaf5c
Imrul Anwar <imrulanwar836@gmail.com>
2024-03-15 08:04:29 +0600

    Intial Commit

--- app/src/main/java/com/imrul/userauthenticationpractice/MainActivity.kt
+++ app/src/main/java/com/imrul/userauthenticationpractice/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -24,24 +29,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             UserAuthenticationPracticeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = Color.Cyan
-                ) {
-                    Text(
-                        text = "Not Signed In",
-                        textAlign = TextAlign.Center,
-                        color = Color.Red,
-                        modifier = Modifier
-                            .padding(16.dp)
-                            .background(Color.Yellow) // Set the background color here
-                            .padding(16.dp)
-                            .fillMaxWidth()
-                    )
+                Box(Modifier.fillMaxSize().background(Color.Gray)) {
+                    Column(
+                        verticalArrangement = Arrangement.SpaceEvenly,
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(text = "Not Signed In")
+                        Button(onClick = { signIn() }){
+                            Text(text = "Sign in")
+                        }
+                        Button(onClick = { signIn() }){
+                            Text(text = "Sign Out")
+                        }
+                    }
                 }
             }
         }
     }
+}
+
+fun signIn() {
+
 }
 
 @Composable
